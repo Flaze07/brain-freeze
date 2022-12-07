1d4eae349a967b27d0eff83b5243abb6a5cdf633
christianseiji.cs@gmail.com <Ch28871c33@#>
2022-12-07 13:01:56 +0700

    feat: created utility function
    the utility functions consists of:
    ord, converts char to ascii
    chr, converts ascii to char

---
+++ ui/util.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {string} val 
+ * @returns number
+ */
+function ord(val) {
+    return val.charCodeAt(0);
+}
+
+/**
+ * 
+ * @param {number} val 
+ * @returns string
+ */
+function chr(val) {
+    return String.fromCharCode(val);
+}
